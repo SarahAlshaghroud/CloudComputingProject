8cf07df7ecd97cc4b7af95cf704cdddc1185836d
SarahAlshaghroud <99715100+SarahAlshaghroud@users.noreply.github.com>
2022-12-23 22:46:51 +0300

    Delete sub-info.sh

--- sub-info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker service inspect --format='{{.ID}}' subscriber
-docker service inspect --format='{{.CreatedAt}}' subscriber
-docker service inspect --format='{{.Spec.Name}}' subscriber
-

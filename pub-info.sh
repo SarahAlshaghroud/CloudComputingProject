be4d992ea1889f85d9109d44b8d6ddeaada8237e
SarahAlshaghroud <99715100+SarahAlshaghroud@users.noreply.github.com>
2022-12-23 22:46:45 +0300

    Delete pub-info.sh

--- pub-info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker service inspect --format='{{.ID}}' publisher
-docker service inspect --format='{{.CreatedAt}}' publisher
-docker service inspect --format='{{.Spec.Name}}' publisher
-
